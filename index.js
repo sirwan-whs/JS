7685909518395adefc6145c8d4e839f3df9f51af
Sirwan Rashek <sirwan.rashek@studmail.w-hs.de>
2026-06-16 11:09:16 +0200

    Wiederholung von JS

---
+++ index.js
@@ -0,0 +1,8 @@
+
+        // This is a simple JavaScript code that
+        // logs "Hello, World!" to the console
+        console.log("Hello, World!");
+   
+    let name;
+    name = "John Doe";
+    console.log(name); // Output: undefined
