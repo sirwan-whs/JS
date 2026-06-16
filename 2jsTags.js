7ae6b68c4c9992d4fa11e6ed670b08f860e35430
Sirwan Rashek <sirwan.rashek@studmail.w-hs.de>
2026-06-16 19:07:57 +0200

    JavaScript tags

---
+++ 2jsTags.js
@@ -0,0 +1,9 @@
+
+let text = document.getElementById("demo");
+let button = document.getElementById("myButton");
+
+    button.onclick = function() {
+    text.innerHTML = "Paragraph changed."; // document.getElementById("demo")
+    };
+
+    
