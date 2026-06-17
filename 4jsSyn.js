0cf94199fd8dba0dfa9febec86c4d2d374ffec32
Sirwan Rashek <sirwan.rashek@studmail.w-hs.de>
2026-06-17 13:09:42 +0200

    JavaScript Syntax

---
+++ 4jsSyn.js
@@ -0,0 +1,25 @@
+// How to Declare variables:
+let x = 5;
+let y = 6;
+
+// How to Compute values:
+let z = x + y;
+
+print(x);
+print(y);
+print(z);
+
+
+document.getElementById("demo").innerHTML = 10.50;
+
+
+let x1 = 31;
+const fname = "Sirwan"
+
+document.getElementById("demo2").innerHTML = fname + " und ich bin "+ x1 + " Jahre alt.";
+
+let a = 5;
+let b = 6;
+let c = a + b;
+document.getElementById("demo3").innerHTML = "a + b = " + c;
+
